42babd85cad95c5ffbf3ff73e31b71a9a74ced99
Chirag Jain <75698873+chiragjain307@users.noreply.github.com>
2024-03-02 01:58:32 +0530

    Update Services.jsx

--- src/Components/Content/Services.jsx
+++ src/Components/Content/Services.jsx
@@ -13,7 +13,7 @@ function Services() {
   }, [1000])
   return (
     <div id='Services' className='p-20 flex flex-col items-center justify-center'>
-      <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-500'>Services</h1>
+      <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-500'>Tech Stack</h1>
       <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-20'>
 
         <h2 data-aos='fade-up' className='text-[26px] flex items-center justify-center font-semibold text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 border-glow'>
