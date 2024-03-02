3f06cab8935b6b2676d6efb1b16e24f51598d1d7
chiragjain307 <chiragjain.jain7@gmail.com>
2024-03-03 01:16:38 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,44 +4,35 @@ import { FaLinkedinIn, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <footer className='bg-slate-800 text-white lg:px-48 px-4 py-20'>
-            <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4'>
-                <div className='mb-4 md:mb-0'>
-                    <span className='text-3xl font-semibold text-fuchsia-800 py-2 uppercase'>LOGO</span>
-                    <p className='text-[16px] my-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, maiores.</p>
-                </div>
+
+        <footer className='md:flex md:px-10 lg:flex bg-slate-800 text-white lg:px-48 px-4 py-5 justify-between'>
                 <div>
-                    <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2 uppercase'>TechStack</h2>
-                    <ul className='text-[16px] my-4'>
-                        <li className='my-2'>Web Design</li>
-                        <li className='my-2'>Web Development</li>
-                        <li className='my-2'>SEO</li>
-                        <li className='my-2'>E-commerce</li>
-                    </ul>
+                    <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2'>Copyright © 2024. All rights are reserved</h2>
                 </div>
-                <div className='mb-4 md:mb-0'>
-                    <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2 uppercase'>Contact</h2>
-                    <p className='text-[16px] my-4'>Email: chiragjain.jain7@gmail.com</p>
-                    <p className='text-[16px] my-4'>Phone: +91-8302421632</p>
-                </div>
-                <div>
-                    <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2 uppercase'>Follow Me</h2>
-                    <div className='flex space-x-4'>
-                        <a href="https://www.linkedin.com/in/chirag-jain-77aa30148" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
-                            <FaLinkedinIn className='text-[28px]' />
-                        </a>
-                        <a href="https://github.com/chiragjain307" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
-                            <AiFillGithub className='text-[28px]' />
-                        </a>
-                        <a href="https://twitter.com/mr_kasliwal" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
-                            <FaTwitter className='text-[28px]' />
-                        </a>
-                        <a href="https://www.instagram.com/_mr_kasliwal_" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
-                            <FaInstagram className='text-[28px]' />
-                        </a>
+                <div className='md:flex lg:flex gap-10 '>
+                    <div>
+                        <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2  uppercase'>Contact</h2>
+                        <a href="mailto:chiragjain.jain7@gmail.com" className='text-[16px] text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out cursor-pointer'>Email: chiragjain.jain7@gmail.com</a>
+                        <p className='text-[16px] text-white pb-5 hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>Phone: +91-8302421632</p>
+                    </div>
+                    <div>
+                        <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2 uppercase'>Follow Me</h2>
+                        <div className='flex space-x-4'>
+                            <a href="https://www.linkedin.com/in/chirag-jain-77aa30148" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+                                <FaLinkedinIn className='text-[28px]' />
+                            </a>
+                            <a href="https://github.com/chiragjain307" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+                                <AiFillGithub className='text-[28px]' />
+                            </a>
+                            <a href="https://twitter.com/mr_kasliwal" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+                                <FaTwitter className='text-[28px]' />
+                            </a>
+                            <a href="https://www.instagram.com/_mr_kasliwal_" className='text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+                                <FaInstagram className='text-[28px]' />
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
         </footer>
     )
 }
