10b2eb1f7fe3f5c47d3bee7d345e50e1c285efbb
chiragjain307 <chiragjain.jain7@gmail.com>
2024-03-03 16:16:53 +0530

    weather project

--- src/Components/Content/Projects.jsx
+++ src/Components/Content/Projects.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import img1 from '../../assets/img1.png'
-import img2 from '../../assets/todoApp.png'
-import img3 from '../../assets/img3.png'
+import img2 from '../../assets/weather.png'
+import img3 from '../../assets/todoApp.png'
 import img4 from '../../assets/img4.png'
 import { AiFillGithub } from 'react-icons/ai'
 
@@ -29,24 +29,24 @@ function Projects() {
         </div>
 
         <div data-aos='fade-down' className='flex flex-col'>
-          <a href="https://todo-web-app-omega.vercel.app">
+          <a href="https://weather-webapp-pro.vercel.app/">
             <img className='text-[26px] flex items-center justify-center rounded-3xl h-40 w-60 p-1 border-2 border-fuchsia-800 border-glow' src={img2} alt="project" />
           </a>
           <p className='flex gap-2 text-center h-full lg:mt-4 lg:items-center lg:justify-center mt-4 text-white'>
-            Todo WebApp
-            <a href="https://github.com/chiragjain307/Todo-WebApp" className='text-center text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+            Current Weather WebApp
+            <a href="https://github.com/chiragjain307/Weather-WebApp" className='text-center text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
               <AiFillGithub className='text-[20px]' />
             </a>
           </p>
         </div>
 
         <div data-aos='fade-up' className='flex flex-col'>
-          <a href="">
+          <a href="https://todo-webapp-pro.vercel.app/">
             <img className='text-[26px] flex items-center justify-center rounded-3xl h-40 w-60 p-1 border-2 border-fuchsia-800 border-glow' src={img3} alt="project" />
           </a>
           <p className='flex gap-2 text-center h-full lg:mt-4 lg:items-center lg:justify-center mt-4 text-white'>
-            2nd Project
-            <a href="" className='text-center text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
+            Todo WebApp
+            <a href="https://github.com/chiragjain307/Todo-WebApp" className='text-center text-white hover:text-fuchsia-800 transition-all duration-150 ease-in-out'>
               <AiFillGithub className='text-[20px]' />
             </a>
           </p>
