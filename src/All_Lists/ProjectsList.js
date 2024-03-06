49a982faf28fdcdcbd1582c29811ffe8d4953f27
chiragjain307 <chiragjain.jain7@gmail.com>
2024-03-07 02:52:37 +0530

    images

--- src/All_Lists/ProjectsList.js
+++ src/All_Lists/ProjectsList.js
@@ -1,6 +1,6 @@
 import img1 from '../assets/img1.png'
-import img2 from '../assets/img2.png'
-import img3 from '../assets/img3.png'
+import img2 from '../assets/weather.png'
+import img3 from '../assets/todoApp.png'
 import img4 from '../assets/img4.png'
 
 const ProjectsList = [
