23c0770f7137db9ed6ff86a64c014b353021cc11
chiragjain307 <chiragjain.jain7@gmail.com>
2024-07-07 01:10:38 +0530

    TechStackList update

--- src/Components/Content/TechStack.jsx
+++ src/Components/Content/TechStack.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import TechStackList from '../../All_Lists/TackStackList'
+import TechStackList from '../../All_Lists/TechStackList'
 
 function TechStack() {
 
